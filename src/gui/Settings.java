e6e14bbbae5683e8ad01dcac5f120af2e18e9251
LeeboyOver9000 <pedro.ivo@aluno.uece.br>
2014-12-12 15:19:10 -0200

    Novo Layout

--- src/gui/Settings.java
+++ src/gui/Settings.java
@@ -6,5 +6,5 @@ public class Settings
 	public static final int SCREEN_HEIGHT = 480;
 	
 	/** Return the total width of screen */
-	public static final int SCREEN_WIDTH = 480;
+	public static final int SCREEN_WIDTH = 400;
 }
